e6213dff8df743aa1c042df3aa21142c6275309e
Brooke Lewis <belv2c@gmail.com>
2018-04-03 21:11:30 -0500

    added batches controller and repository

---
+++ AmberAndGrain/Controllers/BatchesController.cs
@@ -0,0 +1,20 @@
+﻿using AmberAndGrain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AmberAndGrain.Controllers
+{
+    [RoutePrefix("api/batches")]
+    public class BatchesController : ApiController
+    {
+        [Route, HttpPost]
+        public HttpResponseMessage AddBatch(AddBatchDto addBatch)
+        {
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ AmberAndGrain/Models/AddBatchDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmberAndGrain.Models
+{
+    public class AddBatchDto
+    {
+        public int RecipeId { get; set; }
+        public string Cooker { get; set; }
+    }
+}
